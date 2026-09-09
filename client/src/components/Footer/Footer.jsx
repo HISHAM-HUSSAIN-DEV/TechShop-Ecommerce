4196c23caf6a1db3090c445b1c3c47cf15ffcc41
Hisham AlSaedi <heshamfuture1900@gmail.com>
2026-09-09 06:12:39 +0300

    Add github link

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -94,7 +94,7 @@ const Footer = () => {
           {/* Social */}
           <div className="flex items-center gap-3">
             <a
-              href="https://github.com/HISHAM-HUSSAIN-DEV"
+              href="https://github.com/HISHAM-HUSSAIN-DEV/TechShop-Ecommerce"
               target="_blank"
               rel="noopener noreferrer"
               aria-label="GitHub"
